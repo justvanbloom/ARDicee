29303177d0cce5a6a852e43eccaf9d101f063935
Valeria Heikkila <valeria.heikkila@gmail.com>
2020-12-29 14:46:37 +0900

    Delete default methods from View Controller

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -56,19 +56,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
